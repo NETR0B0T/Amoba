584d164fa92d1a843423063aa1b1d9e159e6a15f
NETR0B0T <pctam03@gmail.com>
2024-05-19 22:38:07 +0200

    MainWindow

---
+++ MainWindow.h
@@ -0,0 +1,17 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <vector>
+#include "graphics.hpp"
+
+class widget;
+
+class MainWindow{
+        protected:
+        std::vector<widget*> widgets;
+    public:
+        MainWindow(int XX, int YY);
+        ~MainWindow()=default;
+        void hozzaad(widget*);
+        void loop();
+    };
+#endif // MAINWINDOW_H
